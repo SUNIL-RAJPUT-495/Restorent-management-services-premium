5adeaabe1ff04a32c56ca03f742c8b9f0972c674
sunil-495 <sunilsinghshekhawat495@gmail.com>
2026-05-05 12:54:22 +0530

    updatedddd

--- api/src/routes/Super_Admin/restorentRouter.js
+++ api/src/routes/Super_Admin/restorentRouter.js
@@ -4,7 +4,7 @@ const restorentRouter = express.Router();
 import { create_restaurant, get_all_restaurants } from "../../controllers/Super_Admin/RestorentController.js";
 import { Super_Admin_authMiddleware } from "../../middlewares/Super_Admin_authMiddleware.js";
 
-restorentRouter.post("/create", Super_Admin_authMiddleware, create_restaurant);
+restorentRouter.post("/create",  create_restaurant);
 restorentRouter.get("/all", Super_Admin_authMiddleware, get_all_restaurants);
 
 export default restorentRouter;
