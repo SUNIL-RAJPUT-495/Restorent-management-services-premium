c38d96cfe7bee044d713a47ea155ccd7d161af3d
sunil-495 <sunilsinghshekhawat495@gmail.com>
2026-05-05 12:48:54 +0530

    updateddd url

--- Client/src/common/SummaryApi.jsx
+++ Client/src/common/SummaryApi.jsx
@@ -1,4 +1,4 @@
-export const baseURL = import.meta.env.VITE_API_URL || "https://restorent-management-services-premium-xj81-joeyphkqt.vercel.app";
+export const baseURL = import.meta.env.VITE_API_URL || "https://restorent-management-services-premi-three.vercel.app";
 
 const SummaryApi = {
     // SuperAdmin
